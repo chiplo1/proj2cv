a2165f99ac78bfdb21858659c3c3bc90f1f7cb71
chiplo1 <guilhermechiplo@gmail.com>
2020-01-07 17:23:35 +0000

    trackbar for brightness and constrast added

--- trackbar.cpp
+++ trackbar.cpp
@@ -1,34 +1,53 @@
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
+
+using namespace std;
 using namespace cv;
-using std::cout;
-const int alpha_slider_max = 100;
-int alpha_slider;
-double alpha;
-double beta;
-Mat src1;
-Mat src2;
-Mat dst;
-static void on_trackbar( int, void* )
+
+int main( int argc, char** argv )
 {
-   alpha = (double) alpha_slider/alpha_slider_max ;
-   beta = ( 1.0 - alpha );
-   addWeighted( src1, alpha, src2, beta, 0.0, dst);
-   imshow( "Linear Blend", dst );
-}
-int main( void )
-{
-   src1 = imread( ("youtube.png") );
-   src2 = imread( ("zorin.png") );
-   if( src1.empty() ) { cout << "Error loading src1 \n"; return -1; }
-   if( src2.empty() ) { cout << "Error loading src2 \n"; return -1; }
-   alpha_slider = 0;
-   namedWindow("Linear Blend", WINDOW_AUTOSIZE); // Create Window
-   char TrackbarName[50];
-   sprintf( TrackbarName, "Alpha x %d", alpha_slider_max );
-   createTrackbar( TrackbarName, "Linear Blend", &alpha_slider, alpha_slider_max, on_trackbar );
-   on_trackbar( alpha_slider, 0 );
-   waitKey(0);
-   return 0;
+     // Read original image 
+     Mat src = imread("youtube.png");
+
+     //if fail to read the image
+     if (!src.data) 
+     { 
+          cout << "Error loading the image" << endl;
+          return -1; 
+     }
+
+     // Create a window
+     namedWindow("My Window", 1);
+
+     //Create trackbar to change brightness
+     int iSliderValue1 = 50;
+     createTrackbar("Brightness", "My Window", &iSliderValue1, 100);
+
+     //Create trackbar to change contrast
+     int iSliderValue2 = 50;
+     createTrackbar("Contrast", "My Window", &iSliderValue2, 100);
+
+     while (true)
+     {
+          //Change the brightness and contrast of the image (For more infomation http://opencv-srf.blogspot.com/2013/07/change-contrast-of-image-or-video.html)
+          Mat dst;
+          int iBrightness  = iSliderValue1 - 50;
+          double dContrast = iSliderValue2 / 50.0;
+          src.convertTo(dst, -1, dContrast, iBrightness); 
+
+          //show the brightness and contrast adjusted image
+          imshow("My Window", dst);
+
+          // Wait until user press some key for 50ms
+          int iKey = waitKey(50);
+
+          //if user press 'ESC' key
+          if (iKey == 27)
+          {
+               break;
+          }
+     }
+
+     return 0;
 }
